adc7e31a31bd367ff1cf08271dbe0a8e019abfe3
Alex Smith <altaudiolabs@gmail.com>
2019-12-29 13:23:36 +0000

    Test deploy withour H1 imported

--- components/composed/PageHeader.js
+++ components/composed/PageHeader.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { string } from "prop-types";
 import { Header } from "core/Header";
-import { H1 } from "core/H1";
+// import { H1 } from "core/H1";
 
 const PageHeader = ({ title }) => {
   return (
     <Header>
-      <H1>{title}</H1>
+      <h1>{title}</h1>
     </Header>
   );
 };
