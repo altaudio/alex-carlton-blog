d150167728aaec89220e367fc423492adef15d96
Alex Smith <altaudiolabs@gmail.com>
2019-12-29 13:42:58 +0000

    Captilises headers filepath

--- components/composed/PageHeader.js
+++ components/composed/PageHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { string } from "prop-types";
 import { Header } from "core/Header";
-import { H1 } from "core/headers/HeaderOne";
+import { H1 } from "core/Headers/HeaderOne";
 
 const PageHeader = ({ title }) => {
   return (
