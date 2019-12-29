0fdbe35b58b2115ae943bc561bf54cb57d54f9df
Alex Smith <altaudiolabs@gmail.com>
2019-12-29 13:26:38 +0000

    Adds import

--- components/composed/PageHeader.js
+++ components/composed/PageHeader.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { string } from "prop-types";
 import { Header } from "core/Header";
-// import { H1 } from "core/H1";
+import { H1 } from "core/H1";
 
 const PageHeader = ({ title }) => {
   return (
     <Header>
-      <h1>{title}</h1>
+      <H1>{title}</H1>
     </Header>
   );
 };
