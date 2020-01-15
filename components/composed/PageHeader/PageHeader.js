aab420dfa80d3f3c634d803c7b56ac63431cefe4
Alex Smith <altaudiolabs@gmail.com>
2020-01-15 19:55:14 +0000

    Removed uneeded navigation

--- components/composed/PageHeader/PageHeader.js
+++ components/composed/PageHeader/PageHeader.js
@@ -2,13 +2,11 @@ import React from "react";
 import { string } from "prop-types";
 import { Header } from "core/Header";
 import { H1 } from "core/Headers/HeaderOne";
-import { Navigation } from "./Navigation";
 
 const PageHeader = ({ title }) => {
   return (
     <Header>
       <H1>{title}</H1>
-      <Navigation />
     </Header>
   );
 };
